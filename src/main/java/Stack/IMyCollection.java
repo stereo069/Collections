dd3ec654975745a218f4e6930663d8a4a4dd1511
diasonov <stereo069@gmail.com>
2017-02-12 22:20:25 +0300

    add interface IMyCollection

---
+++ src/main/java/Stack/IMyCollection.java
@@ -0,0 +1,39 @@
+package Stack;
+
+
+import java.util.Iterator;
+
+public interface IMyCollection<T> {
+
+
+    /*
+     Search by key. Return true if was found node.
+     @param: T key
+     @return: boolean
+     */
+    boolean search(T key);
+
+    /*
+     Remove by key. Return true if was deleted at least one node.
+     @param: T key
+     @return: boolean
+     */
+    boolean remove(T key);
+
+    /*
+      Remove all nodes;
+      @param:  void
+      @return: void
+     */
+    void clear();
+
+
+    /*
+       on the void check
+       @param: void
+       @return: boolean
+     */
+    boolean isEmpty();
+
+
+}
